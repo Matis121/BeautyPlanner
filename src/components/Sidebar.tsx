2d34dbfd79b8752b481c52fc145b676e485104c0
Matis121 <mateusz6246@gmail.com>
2023-08-23 20:46:06 +0200

    feat: sidebar active links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, NavNavLink } from "react-router-dom";
 import {
   LuCalendarDays,
   LuUsers,
@@ -12,49 +12,49 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const Sidebar = () => {
   return (
     <>
-      <section className="flex flex-col justify-between items-center bg-gray-200 h-screen w-26 z-50 px-2 py-5">
+      <nav className="flex flex-col justify-between items-center bg-gray-200 h-screen w-26 z-50 px-2 py-5">
         <div className="flex flex-col items-center">
           <Avatar className="shadow-md hover:cursor-pointer hover:shadow-xl hover:scale-105 mb-6">
             <AvatarImage src="https://lh6.googleusercontent.com/-rHKzuuteqoc/AAAAAAAAAAI/AAAAAAAAAAA/fK12yfqT0TM/s44-w44-h44-p-k-no-ns-nd/photo.jpg" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <Link
+          <NavLink
             to="/calendar"
             className="my-4 flex flex-col justify-center items-center text-gray-700 hover:cursor-pointer hover:text-gray-500"
           >
             <LuCalendarDays size={28} />
             <span className="mt-1 font-normal text-xs">Kalendarz</span>
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/clients"
             className="my-4 flex flex-col justify-center items-center text-gray-700 hover:cursor-pointer hover:text-gray-500"
           >
             <LuUsers size={28} />
             <span className="mt-1 font-normal text-xs">Klienci</span>
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/services"
             className="my-4 flex flex-col justify-center items-center text-gray-700 hover:cursor-pointer hover:text-gray-500"
           >
             <LuWrench size={28} />
             <span className="mt-1 font-normal text-xs">Usługi</span>
-          </Link>
-          <Link
-            to="/calendar"
+          </NavLink>
+          <NavLink
+            to="/reports"
             className="my-4 flex flex-col justify-center items-center text-gray-700 hover:cursor-pointer hover:text-gray-500"
           >
             <LuLineChart size={28} />
             <span className="mt-1 font-normal text-xs">Raporty</span>
-          </Link>
+          </NavLink>
         </div>
-        <Link
-          to="/calendar"
+        <NavLink
+          to="/settings"
           className="flex flex-col justify-center items-center text-gray-700 hover:cursor-pointer hover:text-gray-500"
         >
           <LuSettings size={28} />
           <span className="mt-1 font-normal text-xs">Ustawienia</span>
-        </Link>
-      </section>
+        </NavLink>
+      </nav>
     </>
   );
 };
