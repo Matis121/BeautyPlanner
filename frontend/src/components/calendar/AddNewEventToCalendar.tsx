a5bdd802b70b0fb6eae003c2714d6400564c137f
Matis121 <mateusz6246@gmail.com>
2023-11-21 01:21:25 +0100

    feat: full validation for addNewEvent

--- frontend/src/components/calendar/AddNewEventToCalendar.tsx
+++ frontend/src/components/calendar/AddNewEventToCalendar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import {
   Select,
@@ -35,13 +35,55 @@ const AddNewEventToCalendar = props => {
   const userData = JSON.parse(userToken).username;
 
   // EVENT VALUES
-  const [eventTitle, setEventTitle] = useState("");
-  const [eventDescription, setEventDescription] = useState("");
+  const [selectClient, setSelectClient] = useState("");
+  const [selectService, setSelectService] = useState("");
   const [freeTime, setFreeTime] = useState(false);
 
   // VARIABLES FOR END TIME EVENT
-  const [serviceDuration, setServiceDuration] = useState(null);
-  const startTimeStr = props.startTimeEvent;
+  const startTimeStr = new Date(props.startTimeEvent);
+
+  // VARIABLES FOR EVENT START AND END TIME
+  const [eventStartTime, setEventStartTime] = useState("");
+  const [eventEndTime, setEventEndTime] = useState("");
+
+  // VALIDATION ERROR STATES
+  const [clientError, setClientError] = useState(false);
+  const [serviceError, setServiceError] = useState(false);
+  const [timeError, setTimeError] = useState({ start: false, end: false });
+
+  // HANDLERS
+  const handleEventTime = duration => {
+    const date = new Date(startTimeStr);
+    function addMinutes(date, minutes) {
+      const newDate = new Date(date.getTime());
+
+      // Calculate hours and remaining minutes
+      const hoursToAdd = Math.floor(minutes / 60);
+      const remainingMinutes = minutes % 60;
+
+      // Adjust hours
+      newDate.setHours(newDate.getHours() + hoursToAdd);
+
+      // Adjust minutes
+      newDate.setMinutes(newDate.getMinutes() + remainingMinutes);
+
+      return newDate;
+    }
+    const endTime = addMinutes(date, duration);
+
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false, // Use 24-hour format
+    };
+    const formattedStartTime = startTimeStr.toLocaleTimeString(
+      "pl-pl",
+      options
+    );
+    const formattedEndTime = endTime.toLocaleTimeString("pl-pl", options);
+    setEventStartTime(formattedStartTime);
+    setEventEndTime(formattedEndTime);
+  };
 
   // FETCH DATA
   const { data: clientsData } = useQuery(["clients"], () =>
@@ -61,10 +103,17 @@ const AddNewEventToCalendar = props => {
   };
 
   // RESET FUNCTION
-  const resetValues = () => {
-    setEventTitle("");
-    setEventDescription("");
-    setFreeTime(false);
+  const resetValues = value => {
+    if (value !== "without-free") {
+      setFreeTime(false);
+    }
+    setSelectClient("");
+    setSelectService("");
+    setEventStartTime("");
+    setEventEndTime("");
+    setClientError(false);
+    setServiceError(false);
+    setTimeError(prev => ({ ...prev, start: false, end: false }));
   };
 
   // MUTATION
@@ -72,21 +121,49 @@ const AddNewEventToCalendar = props => {
     addEvent(userData, eventStructure)
   );
 
+  // MAIN ADD NEW EVENT FUNCTION
   const newHandleAddEvents = async () => {
     let eventStructure;
-    if (freeTime === false && eventTitle === "") {
+
+    //VALIDATION FUNCTIONS
+    if (freeTime === false && (selectClient === "" || selectService === "")) {
+      if (selectClient === "") {
+        setClientError(true);
+      }
+      if (selectService === "") {
+        setServiceError(true);
+      }
       return;
     }
-    const endTimeEvent = new Date(startTimeStr);
-    endTimeEvent.setMinutes(
-      endTimeEvent.getMinutes() + parseFloat(serviceDuration)
-    );
-    const formattedDate = endTimeEvent.toISOString();
+    if (freeTime === true && (eventStartTime === "" || eventEndTime === "")) {
+      if (eventStartTime === "") {
+        setTimeError(prev => ({ ...prev, start: true }));
+      }
+      if (eventEndTime === "") {
+        setTimeError(prev => ({ ...prev, end: true }));
+      }
+      return;
+    }
+
+    // CREATE TIME AND DATE
+    const startDateStr = new Date(startTimeStr);
+    const endDateStr = new Date(startTimeStr);
+    function createFinalDate(selectDate, timeValue) {
+      const [hours, minutes] = timeValue.split(":").map(Number);
+      selectDate.setHours(hours);
+      selectDate.setMinutes(minutes);
+      console.log(selectDate);
+    }
+
+    createFinalDate(startDateStr, eventStartTime);
+    createFinalDate(endDateStr, eventEndTime);
+
+    // CREATING FINAL STRUCTURE FOR REQUEST
     if (freeTime === true) {
       eventStructure = {
         id: crypto.randomUUID(),
-        start: props.startTimeEvent,
-        end: formattedDate,
+        start: startDateStr,
+        end: endDateStr,
         title: "🌴 CZAS WOLNY 🌴",
         description: "",
         freeTime: freeTime,
@@ -94,13 +171,14 @@ const AddNewEventToCalendar = props => {
     } else {
       eventStructure = {
         id: crypto.randomUUID(),
-        start: props.startTimeEvent,
-        end: formattedDate,
-        title: eventTitle,
-        description: eventDescription,
+        start: startDateStr,
+        end: endDateStr,
+        title: selectClient,
+        description: selectService,
         freeTime: freeTime,
       };
     }
+    // CREATE MUTATION AND REQUEST
     try {
       await addNewEventMutation.mutateAsync(eventStructure);
       queryClient.invalidateQueries("events");
@@ -112,25 +190,48 @@ const AddNewEventToCalendar = props => {
     }
   };
 
+  // RESET ON CLOSE FORM
+  useEffect(() => {
+    if (props.openNewEvent) {
+      resetValues();
+    }
+  }, [props.openNewEvent]);
+
+  // FREE DAY
+  const handleFreeDay = () => {
+    setEventStartTime(eventStartTime ? "" : "00:00");
+    setEventEndTime(eventEndTime ? "" : "23:59");
+    setTimeError(prev => ({ ...prev, start: false, end: false }));
+  };
+
   return (
     <div className="">
       <Dialog open={props.openNewEvent} onOpenChange={props.setOpenNewEvent}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Nowa wizyta</DialogTitle>
+            <DialogTitle className="flex gap-6">
+              <button
+                className={freeTime ? `opacity-50` : `opacity-100`}
+                onClick={() => {
+                  setFreeTime(false);
+                  resetValues("without-free");
+                }}
+              >
+                Nowa wizyta
+              </button>
+              <button
+                className={freeTime ? `opacity-100` : `opacity-50`}
+                onClick={() => {
+                  setFreeTime(true);
+                  resetValues("without-free");
+                }}
+              >
+                Czas wolny
+              </button>
+            </DialogTitle>
           </DialogHeader>
           <form>
             <div className="grid gap-4 py-4">
-              <div className="flex items-center gap-2">
-                <Label htmlFor="firstName" className="text-right">
-                  Czas wolny
-                </Label>
-                <Switch
-                  id="airplane-mode"
-                  className="mr-4 col-span-2"
-                  onClick={() => setFreeTime(!freeTime)}
-                />
-              </div>
               {freeTime === false ? (
                 <>
                   <div className="-mb-4">
@@ -140,11 +241,22 @@ const AddNewEventToCalendar = props => {
                     >
                       Klient
                     </Label>
-                    <Select onValueChange={e => setEventTitle(e)} required>
-                      <SelectTrigger className="col-span-2">
+                    <Select
+                      onValueChange={e => {
+                        setSelectClient(e);
+                        setClientError(false);
+                      }}
+                    >
+                      <SelectTrigger
+                        className={
+                          clientError
+                            ? ` border-red-500 text-red-500`
+                            : `border-input`
+                        }
+                      >
                         <SelectValue placeholder="Wybierz klienta" />
                       </SelectTrigger>
-                      <SelectContent className="overflow-y-auto max-h-[13rem]">
+                      <SelectContent className="max-h-[13rem]">
                         <SelectGroup>
                           {clientsData
                             ? clientsData.map(client => (
@@ -175,11 +287,18 @@ const AddNewEventToCalendar = props => {
                         const selectedServiceData = servicesData.find(
                           service => service.name === e
                         );
-                        setServiceDuration(selectedServiceData.duration);
-                        setEventDescription(selectedServiceData.name);
+                        setSelectService(selectedServiceData.name);
+                        handleEventTime(selectedServiceData.duration);
+                        setServiceError(false);
                       }}
                     >
-                      <SelectTrigger className="col-span-3">
+                      <SelectTrigger
+                        className={
+                          serviceError
+                            ? ` border-red-500 text-red-500`
+                            : `border-input`
+                        }
+                      >
                         <SelectValue placeholder="Wybierz usługę" />
                       </SelectTrigger>
                       <SelectContent className="overflow-y-auto max-h-[13rem]">
@@ -198,30 +317,61 @@ const AddNewEventToCalendar = props => {
                       </SelectContent>
                     </Select>
                   </div>
-                  <div>
-                    <Label
-                      htmlFor="lastName"
-                      className="text-xs font-normal text-gray-500"
-                    >
-                      Czas
-                    </Label>
-                    <div className="flex gap-2 items-center">
-                      <Input type="time" className="w-fit" />
-                      <span className="w-5 h-px bg-gray-500"></span>
-                      <Input type="time" className="w-fit" />
-                    </div>
-                  </div>
-                  <div>
-                    <Label
-                      htmlFor="gender"
-                      className="text-xs font-normal text-gray-500"
-                    >
-                      Notatka
-                    </Label>
-                    <Textarea className="max-h-40" />
-                  </div>
                 </>
-              ) : null}
+              ) : (
+                <div className="flex items-center gap-2 mr-4">
+                  <Label htmlFor="firstName" className="text-right">
+                    Cały dzień
+                  </Label>
+                  <Switch className="mr-4 col-span-2" onClick={handleFreeDay} />
+                </div>
+              )}
+              <div>
+                <Label
+                  htmlFor="lastName"
+                  className="text-xs font-normal text-gray-500"
+                >
+                  Czas
+                </Label>
+                <div className="flex gap-2 items-center">
+                  <Input
+                    type="time"
+                    className={`w-fit ${
+                      timeError.start
+                        ? "border-red-500 text-red-500"
+                        : "border-input"
+                    }`}
+                    value={eventStartTime}
+                    onChange={e => {
+                      setEventStartTime(e.target.value);
+                      setTimeError(prev => ({ ...prev, start: false }));
+                    }}
+                  />
+                  <span className="w-5 h-px bg-gray-500"></span>
+                  <Input
+                    type="time"
+                    className={`w-fit ${
+                      timeError.end
+                        ? "border-red-500 text-red-500"
+                        : "border-input"
+                    }`}
+                    value={eventEndTime}
+                    onChange={e => {
+                      setEventEndTime(e.target.value);
+                      setTimeError(prev => ({ ...prev, end: false }));
+                    }}
+                  />
+                </div>
+              </div>
+              <div>
+                <Label
+                  htmlFor="gender"
+                  className="text-xs font-normal text-gray-500"
+                >
+                  Notatka
+                </Label>
+                <Textarea className="max-h-40" />
+              </div>
             </div>
             <div className="flex justify-end gap-4">
               <Button
