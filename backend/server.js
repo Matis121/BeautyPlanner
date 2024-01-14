b069d531d7fb93920f5b7e955531a3cde110a51d
Matis121 <mateusz6246@gmail.com>
2024-01-14 01:52:54 +0100

    fix: add process.env to handle port

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,9 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// API
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
 
 // Routers
