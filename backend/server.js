7113aedec82d846ffb632aa0e91dbbad4fb397b0
Matis121 <mateusz6246@gmail.com>
2024-01-14 02:20:45 +0100

    server variable

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
